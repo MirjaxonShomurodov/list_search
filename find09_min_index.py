6789464d1c27621d6d903091eb6c02e909547efb
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-04 07:58:25 +0500

    9 misol

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,9 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    return  data.index(min(data))
+data=[7,6,5,1,3,9]
+print(find_min_index(data))
+
+
 
