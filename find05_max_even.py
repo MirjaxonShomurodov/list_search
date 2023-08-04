06648552ac1c3899ff30d1db587eae754c792bf4
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-04 08:09:15 +0500

    5 misol

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,11 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    data.sort()
+    for i in range(len(data)):
+        if data[len(data)-i-1]%2==0:
+            return data[len(data)-i-1]
+   
+    
+data=[1,2,3,4,5,6,7,18,4]
+print(find_max_even(data))
